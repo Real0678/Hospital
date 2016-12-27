1d2a8bb63330abe4e53748f8cb38f01b00a52c19
Real0678 <real0678@gmail.com>
2016-12-27 16:17:34 +0600

    second file upload

---
+++ dsave.php
@@ -0,0 +1,17 @@
+
+<html>
+<head>
+<meta name="spm" content="Hosital Management System">
+<title>Save Doctor</title>
+</head>
+<body>
+<table align=center width=750 cellspacing=0 cellpadding=5>
+<tr bgcolor=blue><td align=center><font SIZE=6 color=white>HOSPITAL
+MANAGEMENT SYSTEM</font></td></tr>
+<tr><td><table align=center width=750 cellspacing=0 cellpadding=5>
+<tr><td align=center><a href=dlist.php>Doctors</td><td align=center><a
+href=plist.php>Patients</td><td align=center><a
+href=app.php>Appointments</td>
+</table></td></tr>
+<tr bgcolor=red><td ><font size=4 color=white>Save
+Doctor</font></td></tr>
